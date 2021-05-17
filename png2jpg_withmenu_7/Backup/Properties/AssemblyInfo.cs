ef4789dbd892e9a44d48998c609daf853d1bff37
p1r1 <42385343+p1r1@users.noreply.github.com>
2021-05-17 18:01:42 +0300

    Add files via upload

---
+++ png2jpg_withmenu_7/Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleContextMenu")]
+[assembly: AssemblyDescription("Sample application that demonstrates a simple shell context menu.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleContextMenu")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f174217c-0db6-4c43-af1b-08096c7fc22b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ png2jpg_withmenu_7/Program.cs
@@ -0,0 +1,127 @@
+// Sample application that demonstrates a simple shell context menu.
+// Ralph Arvesen (www.vertigo.com / www.lostsprings.com)
+
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+[assembly: CLSCompliant(true)]
+namespace SimpleContextMenu
+{
+	static class Program
+	{
+		// file type to register
+		const string FileType = "jpegfile";
+
+		// context menu name in the registry
+		const string KeyName = "2jpg";
+
+		// context menu text
+		const string MenuText = "Conver 2jpg";
+
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// process register or unregister commands
+			if (!ProcessCommand(args))
+			{
+				// invoked from shell, process the selected file
+				CopyGrayscaleImage(args[0]);
+			}
+		}
+
+		/// <summary>
+		/// Process command line actions (register or unregister).
+		/// </summary>
+		/// <param name="args">Command line arguments.</param>
+		/// <returns>True if processed an action in the command line.</returns>
+		static bool ProcessCommand(string[] args)
+		{
+			// register
+			if (args.Length == 0 || string.Compare(args[0], "-register", true) == 0)
+			{
+				// full path to self, %L is placeholder for selected file
+				string menuCommand = string.Format(
+					"\"{0}\" \"%L\"", Application.ExecutablePath);
+
+				// register the context menu
+				FileShellExtension.Register(Program.FileType,
+					Program.KeyName, Program.MenuText,
+					menuCommand);
+
+				MessageBox.Show(string.Format(
+					"The {0} shell extension was registered./nMenu olusturulmustur.Resimlere sag tiklayarak cevirebilirsiniz.",
+					Program.KeyName), Program.KeyName);
+
+				return true;
+			}
+
+			// unregister		
+			if (string.Compare(args[0], "-unregister", true) == 0)
+			{
+				// unregister the context menu
+				FileShellExtension.Unregister(Program.FileType, Program.KeyName);
+
+				MessageBox.Show(string.Format(
+					"The {0} shell extension was unregistered.",
+					Program.KeyName), Program.KeyName);
+
+				return true;
+			}
+
+			// command line did not contain an action
+			return false;
+		}
+
+		/// <summary>
+		/// Make a grayscale copy of the image.
+		/// </summary>
+		/// <param name="filePath">Full path to the image to copy.</param>
+		static void CopyGrayscaleImage(string filePath)
+		{
+			try
+			{
+				// full path to the grayscale copy
+				string grayFilePath = Path.Combine(
+					Path.GetDirectoryName(filePath),
+					string.Format("{0} (grayscale){1}",
+					Path.GetFileNameWithoutExtension(filePath),
+					Path.GetExtension(filePath)));
+
+				// using calls Dispose on the objects, important 
+				// so the file is not locked when the app terminates
+				using (Image image = new Bitmap(filePath))
+				using (Bitmap grayImage = new Bitmap(image.Width, image.Height))
+				using (Graphics g = Graphics.FromImage(grayImage))
+				{
+					// setup grayscale matrix
+					ImageAttributes attr = new ImageAttributes();
+					attr.SetColorMatrix(new ColorMatrix(new float[][]{   
+						new float[]{0.3086F,0.3086F,0.3086F,0,0},
+						new float[]{0.6094F,0.6094F,0.6094F,0,0},
+						new float[]{0.082F,0.082F,0.082F,0,0},
+						new float[]{0,0,0,1,0,0},
+						new float[]{0,0,0,0,1,0},
+						new float[]{0,0,0,0,0,1}}));
+
+					// create the grayscale image
+					g.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+						0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attr);
+
+					// save to the file system
+					grayImage.Save(grayFilePath, ImageFormat.Jpeg);
+
+					// success
+					MessageBox.Show(string.Format("Copied grayscale image {0}", grayFilePath), Program.KeyName);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(string.Format("An error occurred: {0}", ex.Message), Program.KeyName);
+				return;
+			}
+		}
+	}
+}
